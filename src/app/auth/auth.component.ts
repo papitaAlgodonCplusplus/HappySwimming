57db6a83a4dddb19b13cebd8919490e6bc02a502
Alexander Quesada <alexander.quesadaquesada@ucr.ac.cr>
2025-03-04 16:27:59 -0600

    refactor(auth): enhance layout and styling, add header component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, HeaderComponent],
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
